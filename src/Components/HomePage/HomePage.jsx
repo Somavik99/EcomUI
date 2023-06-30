0beb8102569250782671ab6391e97029fd8c760d
Avik Roy <somavik99@gmail.com>
2023-06-30 11:13:08 +0530

    main

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+// import React, { useState } from "react";
+import { ImagesThumbnail } from "../../ImageData/ImageData";
 import NavBar from "../NavBar/NavBar";
 
 const HomePage = () => {
-  
+  // const [ImgState, setImageState] = useState([])
+
+  let Thumbnails = [];
+  for (let i = 0; i <= ImagesThumbnail.length; i++) {
+    Thumbnails.push(i);
+  }
+
   return (
     <div>
       <div>
@@ -10,7 +17,9 @@ const HomePage = () => {
           <NavBar />
         </div>
         <div>
-
+          {Thumbnails.map((imgT, index) => {
+            return <div key={index}>{imgT.imageT1}</div>;
+          })}
         </div>
       </div>
     </div>
---
+++ src/ImageData/ImageData.jsx
@@ -0,0 +1,24 @@
+import Shoe1thumbnail from "../Assets/images/Shoe1thumbnail.jpg";
+import Shoe2thumbnail from "../Assets/images/Shoe2thumbnail.jpg";
+import Shoe3thumbnail from "../Assets/images/Shoe3thumbnail.jpg";
+import Shoe4thumbnail from "../Assets/images/Shoe4thumbnail.jpg";
+import Shoe1 from "../Assets/images/Shoe1.jpg";
+import Shoe2 from "../Assets/images/Shoe2.jpg";
+import Shoe3 from "../Assets/images/Shoe3.jpg";
+import Shoe4 from "../Assets/images/Shoe4.jpg";
+
+const ImagesThumbnail = {
+  imageT1: <img src={Shoe1thumbnail} alt="Err" />,
+  imageT2: <img src={Shoe2thumbnail} alt="Err" />,
+  imageT3: <img src={Shoe3thumbnail} alt="Err" />,
+  imageT4: <img src={Shoe4thumbnail} alt="Err" />,
+};
+
+const FullImages = {
+  image1: <img src={Shoe1} alt="ImgErr" />,
+  image2: <img src={Shoe2} alt="ImgErr" />,
+  image3: <img src={Shoe3} alt="ImgErr" />,
+  image4: <img src={Shoe4} alt="ImgErr" />,
+};
+
+export { ImagesThumbnail, FullImages };
