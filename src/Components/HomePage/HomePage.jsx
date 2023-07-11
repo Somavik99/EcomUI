2fc1bbb411e76dadcd0444fd98ddbd85bf87ba72
Avik Roy <somavik99@gmail.com>
2023-07-11 22:59:51 +0530

    main

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import { FullImages } from "../../ImageData/ImageData";
 import NavBar from "../NavBar/NavBar";
 import AddItems from "../DetailsADD/AddItems";
-import {motion} from "framer-motion"
-import "./HomePage.css"
+import { motion } from "framer-motion";
+import { AiOutlineRight, AiOutlineLeft } from "react-icons/ai";
+import "./HomePage.css";
 
 const HomePage = () => {
-  const [ImgState, setImageState] = useState(FullImages[0]);
+  const [current, setCurrent] = useState(0);
+  const [ImgState, setImageState] = useState(FullImages[current]);
 
   let HandleImageChange = (images) => {
     setTimeout(() => {
@@ -16,42 +18,50 @@ const HomePage = () => {
 
   return (
     <>
-       <div style={{paddingLeft:"10%",paddingRight:"10%"}}>
-          <NavBar />
-        </div>
-    <div className="Home">
-    
-      <div className="Home__cont">
-        <div>
-          <motion.div className="FullImage">
-            <img
-              src={ImgState.image}
-              alt="Sneakers"
-              style={{ height: "485px", width: "485px" }}
-              className="fImg"
-              
-            />
-          </motion.div> 
-          <div className="Img__cont" >
-          {FullImages.map((imgT, index) => {
-            return (
-              <div key={index}  className="tImg">
-                <img
-                  src={imgT.image}
-                  alt="err"
-                  style={{ height: "100px", width: "100px",borderRadius:"20px" }}
-                  onClick={() => HandleImageChange(imgT)}
-                />
-              </div>
-            );
-          })}
+      <div style={{ paddingLeft: "10%", paddingRight: "10%" }}>
+        <NavBar />
+      </div>
+      <div className="Home">
+        <div className="Home__cont">
+          <div>
+            <motion.div className="FullImage">
+              <img
+                src={ImgState.image}
+                alt="Sneakers"
+                style={{ height: "485px", width: "485px" }}
+                className="fImg"
+              />
+            </motion.div>
+            <div className="Img__cont">
+              <button>
+                <AiOutlineLeft />
+              </button>
+              {FullImages.map((imgT, index) => {
+                return (
+                  <div key={index} className="tImg">
+                    <img
+                      src={imgT.image}
+                      alt="err"
+                      style={{
+                        height: "100px",
+                        width: "100px",
+                        borderRadius: "20px",
+                      }}
+                      onClick={() => HandleImageChange(imgT)}
+                    />
+                  </div>
+                );
+              })}
+              <button>
+                <AiOutlineRight />
+              </button>
+            </div>
+          </div>
+          <div className="Add__cont">
+            <AddItems />
           </div>
         </div>
-        <div className="Add__cont">
-          <AddItems/>
-        </div>
       </div>
-    </div>
     </>
   );
 };
